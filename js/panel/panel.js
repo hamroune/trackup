ca8e5fee97e131703915c289c52f2f7554bfb724
ninouz ninou <fedoulmednadjib@gmail.com>
2014-11-03 10:54:53 +0100

    disable enable

--- js/panel/panel.js
+++ js/panel/panel.js
@@ -1,6 +1,6 @@
 
 function do_something(msg) {
-    document.body.innerHTML = "</br> <span>"+JSON.stringify(msg)+"</span>"; // Stupid example, PoC
+    document.body.innerHTML = document.body.innerHTML + "</br> <span>"+JSON.stringify(msg)+"</span>"; // Stupid example, PoC
 }
 document.documentElement.onclick = function() {
 	//alert('nother stupid example SENT')
@@ -8,3 +8,22 @@ document.documentElement.onclick = function() {
     // the panel can only be clicked when it's visible...
     respond('Another stupid example!');
 };
+
+
+function disable(){
+	var p = document.createElement("style");
+	
+	// l'ajoute à la fin du corps du document
+	document.body.appendChild(p);
+	var t = document.getElementsByTagName("style");
+	
+	t[t.length-1].innerHTML='* { pointer-events: none; cursor: default; } ';
+	alert("disable");
+}  
+
+  
+function enable(){
+t = document.getElementsByTagName("style");
+t[t.length-1].innerHTML = "";	
+alert("enable");
+} 
